9c957d373a5c7e10a6c4c1db29b0c5895c37e31e
thai <thaithaimimi@gmail.com>
2026-09-02 08:53:15 +0700

    Update target ipq60xx: JDC AX6600, support NSS offload

--- target/linux/qualcommax/image/ipq60xx.mk
+++ target/linux/qualcommax/image/ipq60xx.mk
@@ -90,13 +90,15 @@ TARGET_DEVICES += glinet_gl-axt1800
 
 define Device/jdcloud_re-cs-02
 	$(call Device/FitImage)
+	$(call Device/EmmcImage)
 	DEVICE_VENDOR := JDCloud
 	DEVICE_MODEL := RE-CS-02
 	SOC := ipq6010
-	BLOCKSIZE := 64k
 	KERNEL_SIZE := 6144k
+	BLOCKSIZE := 64k
 	DEVICE_DTS_CONFIG := config@cp03-c3
 	DEVICE_PACKAGES := ath11k-firmware-qcn9074 ipq-wifi-jdcloud_re-cs-02 kmod-ath11k-pci
+	IMAGE/factory.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-rootfs | append-metadata
 endef
 TARGET_DEVICES += jdcloud_re-cs-02
 
